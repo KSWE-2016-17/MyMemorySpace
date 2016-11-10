864f7f74075749bb0c3a9c52cd9f3f9cc991faaf
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-10 21:16:51 +0100

    Add schemas:
    userSchema
    positionSchema
    lightSchema

--- scripts/mongooseSchemas.js
+++ scripts/mongooseSchemas.js
@@ -1,4 +1,32 @@
 /**
  * Created by xenia on 10.11.16.
  */
-import mongoose from 'mongooseqqq'
+import mongoose from 'mongoose';
+
+var userSchema = new mongoose.Schema({
+    username: {type: String, trim: true},
+    password: {type: String, trim: true}
+});
+
+var positionSchema=new mongoose.Schema({
+    x: Number,
+    y: Number,
+    z: Number
+});
+var lightSchema=new mongoose.Schema({
+    position: positionSchema,
+    angle: Number,
+    color: String,
+    intensity: Number,
+    type: String
+});
+var mediafileSchema=new mongoose.Schema({
+
+
+});
+var wallSchema=new mongoose.Schema({
+
+});
+var roomSchema=new mongoose.Schema({
+
+});
