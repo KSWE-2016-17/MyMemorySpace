8bf623e17bc7ecb7ef66aae4f8bc027e11f1b51d
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-15 21:14:34 +0100

    Add REST API for User shema

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,10 @@ server.listen(server.get('port'), function(){
 	console.log('Server running: localhost:'+server.get('port'));
 });
 
-// ADD NEW USER
+// USER
+/*
+* POST: create new user
+* */
 server.post("/user", function(req,res){
 	var user = new dbShema.User({
 		username: req.body.username,
@@ -39,14 +42,61 @@ server.post("/user", function(req,res){
 	});
 });
 
-// GET
-server.get('/user',function(req,res){
+/*
+* GET: get all users
+* */
+server.get('/userlist',function(req,res){
 	dbShema.User.find({}, function(err,result){
 		if(err) throw err;
 		res.json(result);
 	});
 });
 
+/*
+* GET: get one user by _id
+* */
+server.get('/user/:_id',function(req,res){
+	dbShema.User.findById(req.params._id, function(err,result){
+		if(err) throw err;
+		res.json(result);
+	});
+});
 
+/*
+* GET: get one user by name
+* */
+server.get('/user/:name',function(req,res){
+	dbShema.User.findOne(req.params.name, function(err,result){
+		if(err) throw err;
+		res.json(result);
+	});
+});
+
+/*
+* PUT: update user
+* */
+server.put('/user/:_id', function (req,res) {
+	User.
+	dbShema.User.findByIdAndUpdate(req.params._id, function (err,result) {
+		if ( err ) throw err;
+		res.json({
+			message:"Successfully updated the user",
+			user : result
+		});
+	});
+});
+
+/*
+* DELETE: delete user
+* */
+
+server.delete('/user/:_id', function (req, res) {
+	dbShema.User.findByIdAndRemove({_id: req.params._id}, function (err, result) {
+		res.json({
+			message: "Successfully deleted the user",
+			user: result
+		});
+	});
+});
 
 
