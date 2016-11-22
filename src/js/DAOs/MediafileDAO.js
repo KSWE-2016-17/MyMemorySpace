dc72383b34c423a3374c6c05907895a056585699
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-11-23 00:27:34 +0100

    add mediafile and user- DAOs

--- src/js/DAOs/MediafileDAO.js
+++ src/js/DAOs/MediafileDAO.js
@@ -1,3 +1,39 @@
-/**
- * Created by xenia on 22.11.16.
- */
+// schalten und walten 
+// \..../ \..../
+//  \../   \../
+//   \/     \/
+
+
+var DaoHelper = require ("./DaoHelper");
+
+var MediafileDAO = function(connection){
+	this.connection = connection;
+	this.daoHelper = new DaoHelper();
+};
+
+// FIND - GET
+MediafileDAO.prototype.findAll = function(callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl()+"/mediafile", callbacks);
+};
+
+MediafileDAO.prototype.findByID = function(id,callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl()+"/mediafile/"+id, callbacks);
+};
+
+MediafileDAO.prototype.findByUserID = function(id,callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl()+"/mediafile/by_user/"+id, callbacks);
+};
+
+MediafileDAO.prototype.findFileByID = function(id,callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl()+"/mediafile/file/"+id, callbacks);
+};
+// CREATE - POST
+MediafileDAO.prototype.create = function(obj,callbacks){
+	return this.daoHelper.create(obj,this.connection.getFullUrl()+"/mediafile/"+obj._id, callbacks);
+};
+// UPDATE - PUT
+//--
+// DELETE - DELETE 
+MediafileDAO.prototype.delete = function(obj,callbacks){
+	return this.daoHelper.delete(obj,this.connection.getFullUrl()+"/mediafile/"+obj._id, callbacks);
+};
--- src/js/DAOs/UserDAO.js
+++ src/js/DAOs/UserDAO.js
@@ -1,3 +1,34 @@
-/**
- * Created by xenia on 22.11.16.
- */
+var DaoHelper = require ("./DaoHelper");
+
+var UserDAO = function(connection){
+	this.connection = connection;
+	this.daoHelper = new DaoHelper();
+};
+
+// FIND - GET
+UserDAO.prototype.findAll = function(callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl() + "/user", callbacks);
+};
+
+UserDAO.prototype.findByID = function(id,callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl() + "/user/"+id, callbacks);
+};
+
+UserDAO.prototype.findByName = function(name,callbacks){
+	return this.daoHelper.find(this.connection.getFullUrl() + "/user/name/"+name, callbacks);
+};
+
+// CREATE - POST
+UserDAO.prototype.create = function(obj,callbacks){
+	return this.daoHelper.create(obj,this.connection.getFullUrl()+"/user", callbacks);
+};
+
+// UPDATE - PUT
+UserDAO.prototype.update = function(obj,callbacks){
+	return this.daoHelper.update(obj,this.connection.getFullUrl()+"/user/"+obj._id, callbacks);
+};
+
+// DELETE - DELETE 
+UserDAO.prototype.delete = function(obj,callbacks){
+	return this.daoHelper.delete(obj,this.connection.getFullUrl()+"/user/"+obj._id, callbacks);
+};
