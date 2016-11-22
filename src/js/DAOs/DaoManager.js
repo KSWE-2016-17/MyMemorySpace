6615049fc292165d9f572848e572adba28abde39
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-22 14:08:08 +0100

    DAOs: Implement DaoManager

--- src/js/DAOs/DaoManager.js
+++ src/js/DAOs/DaoManager.js
@@ -1,3 +1,29 @@
-/**
- * Created by xenia on 22.11.16.
- */
+var DaoManager = function(){
+    this.daos = {};
+    this.connection = {
+        protocol: "https",
+        url: "localhost",
+        port: "8081"
+    };
+    this.connection.getFullUrl = function() {
+        var result = "";
+
+        result += this.protocol + '//' + this.url + ':' + this.port;
+        console.log("full url to server: " + result);
+        return result;
+    };
+};
+
+DaoManager.prototype.getDao = function(dao) {
+    if (!this.daos[dao]) {
+        this.daos[dao] = this.createDao(dao);
+    }
+
+    return this.daos[dao];
+};
+
+DaoManager.prototype.createDao = function(dao) {
+    return new dao();
+};
+exports.default = DaoManager;
+module.exports = exports.default;
