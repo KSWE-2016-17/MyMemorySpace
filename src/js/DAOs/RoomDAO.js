f38e59b3b8883d0cf07dba6f4bc61c727dcffb8a
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-23 00:31:48 +0100

    Implement RoomDAO

--- src/js/DAOs/RoomDAO.js
+++ src/js/DAOs/RoomDAO.js
@@ -1,3 +1,37 @@
-/**
- * Created by xenia on 22.11.16.
- */
+var DaoHelper = require("./DaoHelper")
+
+var RoomDAO = function(connection){
+    this.connection = connection;
+    this.daoHelper = new DaoHelper();
+};
+RoomDAO.prototype.findAll= function(callbacks){
+    return this.daoHelper.find(this.connection.getFullUrl() + "/room", callbacks);
+};
+
+RoomDAO.prototype.findByUser = function(user_id, callbacks){
+    return this.daoHelper.find(this.connection.getFullUrl() + "/room/by_user/:"+ user_id, callbacks);
+};
+
+RoomDAO.prototype.findById = function(_id, callbacks){
+    return this.daoHelper.find(this.connection.getFullUrl() + "/room/:"+ _id, callbacks);
+};
+RoomDAO.prototype.create = function(obj, callbacks) {
+    return this.daoHelper.create(obj, this.connection.getFullUrl() + "/room", callbacks);
+};
+RoomDAO.prototype.update = function(obj, callbacks) {
+    return this.daoHelper.update(obj, this.connection.getFullUrl() + "/room/:" + obj._id, callbacks);
+};
+
+FriendDAO.prototype.createOrUpdate = function(obj, callbacks) {
+    if (obj._id) {
+        return this.update(obj, this.connection.getFullUrl() + "/room/:" +  obj._id, callbacks);
+    } else {
+        return this.create(obj, this.connection.getFullUrl() + "/room", callbacks);
+    }
+};
+FriendDAO.prototype.delete = function(obj, callbacks) {
+    return this.daoHelper.delete(obj, this.connection.getFullUrl() + "/room/:" +  obj._id , callbacks);
+};
+exports.default = FriendDAO;
+module.exports = exports.default;
+
