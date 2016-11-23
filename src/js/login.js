858172cddf30ec8b0fe5781c2767bc7b7110f553
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-11-23 14:37:17 +0100

    add login page screen

---
+++ src/js/login.js
@@ -0,0 +1,81 @@
+
+console.log("LOADED LOGIN-JS");
+
+let username;
+let password;
+let error;
+
+$(function() {
+  // Handler for .ready() called.
+  console.log("READY $");
+  
+  initButtons();
+  initFormFields();
+  
+  function initButtons(){
+	$("#login").click(login);
+	$("#register").click(register);
+	}
+	
+	function initFormFields(){
+	username = $("#username");
+	password = $("#password");
+	error = $("#error");
+}
+  
+});
+
+
+
+function login(){
+	if(!checkIfFieldEmpty()){
+		sendErrorMessage("Username or Password Field are empty");
+		return;
+	}
+	checkLogin();
+}
+
+function register(){
+	if(!checkIfFieldEmpty()){
+		sendErrorMessage("Username or Password Field are empty");
+		return;
+	}
+	checkRegister();
+}
+
+function checkIfFieldEmpty(){
+	if(username.val() === "" || username.val() === null){
+		console.log("USERNAME EMPTY");
+		return false;
+	}
+	if(password.val() === "" || password.val() === null){
+		console.log("PASSWORD EMPTY");
+		return false;
+	}
+	
+	console.log("Fields are set");
+	
+	return true;
+	
+}
+
+function sendErrorMessage(msg){
+	error.text(msg);
+}
+
+function checkLogin(){
+	let name = username.val();
+	let pass = password.val();
+	
+	//CHECK DIE DB AUF USER 
+	
+}
+
+function checkRegister(){
+	let name = username.val();
+	let pass = password.val();
+	
+	// 1. CHECK OB USERNAME BEREITS SO EXISTIERT
+	// 2. USER REGISTRIEREN
+}
+
