7d6184fc2aec635d6308b2fa605b1dc25c1c8936
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-23 13:45:58 +0100

    Implement structur Room

--- src/js/Room.js
+++ src/js/Room.js
@@ -1,3 +1,12 @@
-/**
- * Created by xenia on 23.11.16.
- */
+var Room = function(id, user_id, roomname, walls, sky, light, mediaobjects){
+    this.id=id;
+    this.user_id=user_id;
+    this.roomname=roomname;
+    this.walls=walls;
+    this.sky=sky;
+    this.light=light;
+    this.mediafile=mediaobjects;
+};
+
+exports.default =Room;
+module.exports = exports.default;
