e957cbed1857e93f8be8ee9c05c9a8b11e3fad26
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-23 14:31:00 +0100

    Change parameter list for Room ctor to room_obj

--- src/js/Room.js
+++ src/js/Room.js
@@ -1,11 +1,11 @@
-var Room = function(id, user_id, roomname, walls, sky, light, mediaobjects){
-    this.id=id;
-    this.user_id=user_id;
-    this.roomname=roomname;
-    this.walls=walls;
-    this.sky=sky;
-    this.light=light;
-    this.mediafile=mediaobjects;
+var Room = function(room_obj){
+    this.id=room_obj.id;
+    this.user_id=room_obj.user_id;
+    this.roomname=room_obj.roomname;
+    this.walls=room_obj.walls;
+    this.sky=room_obj.sky;
+    this.light=room_obj.light;
+    this.mediafile=room_obj.mediaobjects;
 };
 
 exports.default =Room;
