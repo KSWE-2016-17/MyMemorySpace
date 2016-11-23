124e4a97d97f451e8a969391d5f3c0f322c7fca1
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-23 14:11:33 +0100

    Implement structur Mediaobject

---
+++ src/js/Mediaobject.js
@@ -0,0 +1,14 @@
+
+var Mediafile = function(id, position, width, height, depth, rotation, scale, color){
+    this.id=id;
+    this.position=positio;
+    this.width=width;
+    this.height=height;
+    this.depth=depth;
+    this.rotation=rotation;
+    this.scale=scale;
+    this.color=color;
+};
+
+exports.default =User;
+module.exports = exports.default;
