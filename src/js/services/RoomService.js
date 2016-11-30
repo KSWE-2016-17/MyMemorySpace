ea1e3541295187004ef530503a53b98855f92f9c
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-30 12:57:52 +0100

    Implement UserService and RoomService

---
+++ src/js/services/RoomService.js
@@ -0,0 +1,58 @@
+import q from "q";
+
+export default class RoomService {
+    constructor(){
+        this.dm =  new DaoManager();
+        this.roomDao = this.dm.getDao(RoomDAO);
+    }
+    findAll(){
+        let defer = q.defer();
+
+        this.roomDao.findAll()
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    findById(id){
+        let defer = q.defer();
+        this.roomDao.findById(id)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    findByUser(id){
+        let defer = q.defer();
+        this.roomDao.findByUser(id)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    create(obj){
+        let defer = q.defer();
+        this.roomDao.create(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    update(obj){
+        let defer = q.defer();
+        this.roomDao.update(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    createOrUpdate(obj){
+        let defer = q.defer();
+        this.roomDao.createOrUpdate(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    delete(obj){
+        let defer = q.defer();
+        this.roomDao.delete(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+}
