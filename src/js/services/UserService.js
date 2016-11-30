a57159ca376aab9a0af796b8a406b5e5823cf8e5
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2016-11-30 12:57:52 +0100

    Implement UserService and RoomService

---
+++ src/js/services/UserService.js
@@ -0,0 +1,46 @@
+import q from "q";
+
+export default class UserService {
+    constructor(){
+        this.dm =  new DaoManager();
+        this.userDao = this.dm.getDao(UserDAO);
+    }
+    findByName(name){
+		let defer = q.defer();
+
+        this.userDao.findByName(name)
+            .then(defer.resolve)
+            .catch(defer.reject);
+
+        return defer.promise;
+	}
+	findAll(){
+        let defer = q.defer();
+
+         this.userDao.findAll()
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+	}
+	findById(id){
+        let defer = q.defer();
+        this.userDao.findById(id)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+	}
+	create(obj){
+        let defer = q.defer();
+        this.userDao.create(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+    delete(obj){
+       let defer = q.defer();
+        this.userDao.delete(obj)
+            .then(defer.resolve)
+            .catch(defer.reject);
+        return defer.promise;
+    }
+}
