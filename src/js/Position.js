be2c25484d31bb470e378000b44b4e8e620173f4
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2017-01-05 00:32:01 +0100

    Improve Position implementation

--- src/js/Position.js
+++ src/js/Position.js
@@ -1,7 +1,7 @@
 export default class Position {
     constructor(position){
-        this.x=position.x;
-        this.y = position.y;
-        this.z = position.z;
+        this.x=(position.x? position.x: 0);
+        this.y = (position.y? position.y: 0);
+        this.z = (position.z? position.z: 0);
     }
 }
