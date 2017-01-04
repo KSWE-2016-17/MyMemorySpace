b3d97fe4c7dec14f8c3e005772b6293662be13bb
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2017-01-05 00:36:51 +0100

    Improve ctor implementation from Room, setDefaults function

--- src/js/Room.js
+++ src/js/Room.js
@@ -1,11 +1,36 @@
+import RoomService from "./services/RoomService";
+import Wall from "./Wall";
+
+import q from "q";
+
 export default class Room {
-    constructor(room_obj){
-        this._id=room_obj._id;
-        this.user_id=room_obj.user_id;
-        this.roomname=room_obj.roomname;
-        this.walls=room_obj.walls;
-        this.sky=room_obj.sky;
-        this.light=room_obj.light;
-        this.mediafile=room_obj.mediaobjects;
+    constructor(room_obj, user_id){
+        if(room_obj){
+            this._id=room_obj._id;
+            this.user_id=room_obj.user_id;
+            this.roomname=room_obj.roomname;
+            this.walls=room_obj.walls;
+            this.sky=room_obj.sky;
+            this.light=room_obj.light;
+            this.mediaobjects=room_obj.mediaobjects;
+        } else {
+            this.setDefoults(user_id);
+        }
     }
+
+    setId(id){this._id= id;}
+    setUserId(userid){this.user_id=userid;}
+
+    setDefoults(){
+        this._id=null;
+        this.user_id=null;
+        this.roomname="New Room";
+        this.walls=[
+            new Wall('right'), new Wall('left'), new Wall('back'), new Wall('front'), new Wall('up'), new Wall('down')
+        ];
+        this.sky=null;
+        this.light=null
+        this.mediaobjects=[];
+    }
+
 }
