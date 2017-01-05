80f9972e67affe12b7b3600c79e351e839d757ea
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2017-01-05 23:38:18 +0100

    Improve Wall implementation: get, set, default values

--- src/js/Wall.js
+++ src/js/Wall.js
@@ -3,59 +3,173 @@ import q from "q"
 export default class Wall {
     constructor(wall_obj, direction){
         if(wall_obj){
-            this._id=wall_obj._id;
-            this.position=wall_obj.position;
-            this.width=wall_obj.width;
-            this.height=wall_obj.height;
-            this.depth=wall_obj.depth;
-            this.rotation=wall_obj.rotation;
-            this.color=wall_obj.color;
-            this.textur=wall_obj.textur;
-            this.visible=wall_obj.visible;
+            this.setId(wall_obj._id);
+            if(wall_obj.direction){
+                direction = wall_obj.direction;
+            }
+            this.setDefaultPosition(direction);
+            this.setDefaultWidth();
+            this.setDefaultHeight();
+            this.setDefaultDepth();
+            this.setDefaultRotation(direction);
+            this.setColor(wall_obj.color);
+            this.setTextur(wall_obj.textur);
+            this.setVisible(wall_obj.visible);
+            this.setDirection(direction);
         } else {
-            getDefaults(direction);
+            this.setDefaults(direction);
         }
 
     }
-    setDefaults(direction){
-        this._id=null;
-        this.width=2;
-        this.height=40;
-        this.depth=40;
-        this.color='#fff';
-        this.textur=null;
-        this.visible=true;
-
+    getDefaultPosition(direction){
+        let position;
         switch (direction){
             case 'right':
-                this.position=new Position({x:20, y:0, z:0});
-                this.rotation=new Position({x:0, y:0, z:0});
+                position=new Position({x:20, y:0, z:0});
                 break;
             case 'left':
-                this.position=new Position({x:-20, y:0, z:0});
-                this.rotation=new Position({x:0, y:0, z:0});
+                position=new Position({x:-20, y:0, z:0});
                 break;
             case 'back':
-                this.position=new Position({x:0, y:0, z:-20});
-                this.rotation=new Position({x:0,y: 90, z:0});
+                position = this.position=new Position({x:0, y:0, z:-20});
                 break;
             case 'front':
-                this.position=new Position({x:0, y:0, z:20});
-                this.rotation=new Position({x:0, y:90, z:0});
+                position = this.position=new Position({x:0, y:0, z:20});
                 break;
             case 'up':
-                this.position=new Position({x:0,y: 40, z:0});
-                this.rotation=new Position({x:90,y: 0,z: 0});
+                position=new Position({x:0,y: 40, z:0});
                 break;
             case 'down':
-                this.position=new Position({x:0, y:-10, z:0});
-                this.rotation=new Position({x:90, y:0,z: 0});
+                position=new Position({x:0, y:-10, z:0});
                 break;
             default:
-                this.position=new Position({x:0, y:0, z:0});
-                this.rotation=new Position({x:0, y:0,z: 0});
+                position=new Position({x:0, y:0, z:0});
                 break;
         }
+        return position;
+    }
+    getDefaultRotation(direction){
+        let rotation;
+        switch (direction){
+            case 'right':
+                rotation=new Position({x:0, y:0, z:0});
+                break;
+            case 'left':
+                rotation=new Position({x:0, y:0, z:0});
+                break;
+            case 'back':
+                rotation=new Position({x:0,y: 90, z:0});
+                break;
+            case 'front':
+                rotation=new Position({x:0, y:90, z:0});
+                break;
+            case 'up':
+                rotation=new Position({x:90,y: 0,z: 0});
+                break;
+            case 'down':
+                rotation=new Position({x:90, y:0,z: 0});
+                break;
+            default:
+                rotation=new Position({x:0, y:0,z: 0});
+                break;
+        }
+        return rotation;
+    }
+    setDefaultPosition(direction){
+        this.position = this.getDefaultPosition(direction);
+    }
+    setDefaultRotation(direction){
+        this.rotation = this.getDefaultRotation(direction);
+    }
+    getDefaultWidth(){
+        return 2;
+    }
+    getDefaultHeight(){
+        return 40;
+    }
+    getDefaultDepth(){
+        return 40;
+    }
+    getDefaultColor(){
+        return '#fff';
+    }
+    setDefaultWidth(){
+        this.width= 2;
+    }
+    setDefaultHeight(){
+        this.height =40;
+    }
+    setDefaultDepth(){
+        this.depth =  40;
+    }
+    setDefaultColor(){
+        this.color= '#fff';
+    }
 
+    setDefaults(direction){
+        this._id=null;
+        this.width=this.getDefaultWidth();
+        this.height=this.getDefaultHeight();
+        this.depth=this.getDefaultDepth();
+        this.color=this.getDefaultColor();
+        this.textur=null;
+        this.visible=true;
+        this.position = this.getDefaultPosition(direction);
+        this.rotation = this.getDefaultRotation(direction);
+    }
+    setColor(color){
+        if(color){
+            this.color = color;
+        } else {
+            this.color = '#fff';
+        }
+    }
+
+    getColor(){
+        return this.color;
+    }
+
+    setVisible(visibility){
+        if(visibility){
+            this.visible = visibility;
+        } else {
+            this.visible = true;
+        }
+
+    }
+
+    getVisible(){
+        return this.visible;
+    }
+
+    setTextur(texture){
+        if(texture){
+            this.textur = texture;
+        } else {
+            this.textur = null;
+        }
+
+    }
+
+    getTextur(){
+        return this.textur;
+    }
+
+    setDirection(direction){
+        if(direction){
+            this.direction = direction;
+        } else {
+            this.direction = 'right';
+        }
+    }
+    getDirection(){
+        return this.direction;
+    }
+    setId(id){
+        if(id){
+            this._id = id;
+        } else {
+            this._id = null;
+        }
     }
 }
