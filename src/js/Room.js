67ab6f8f2548fd5a456f00dc3c353a5802201c20
oxanaZh <oxana.zhurakovskaya@fh-bielefeld.de>
2017-01-06 15:42:50 +0100

    Improve Room: in ctor paramter settings, Add get and set methods

--- src/js/Room.js
+++ src/js/Room.js
@@ -6,31 +6,99 @@ import q from "q";
 export default class Room {
     constructor(room_obj, user_id){
         if(room_obj){
-            this._id=room_obj._id;
-            this.user_id=room_obj.user_id;
-            this.roomname=room_obj.roomname;
-            this.walls=room_obj.walls;
-            this.sky=room_obj.sky;
-            this.light=room_obj.light;
-            this.mediaobjects=room_obj.mediaobjects;
+            this.setId(room_obj._id);
+            this.setUserId(room_obj.user_id);
+            this.setRoomName(room_obj.roomname);
+            this.setWalls(room_obj.walls);
+            this.setSky(room_obj.sky);
+            this.setLight(room_obj.light);
+            this.setMediaobjects(room_obj.mediaobjects);
         } else {
             this.setDefaults(user_id);
         }
     }
 
-    setId(id){this._id= id;}
-    setUserId(userid){this.user_id=userid;}
+    setId(id){
+        if(id){
+            this._id= id;
+        } else {
+            this._id=null;
+        }
+    }
+    setUserId(userid){
+        if(userid){
+            this.user_id=userid;
+        } else {
+            this.user_id = null;
+        }
+    }
+    setRoomName(name){
+        if(name){
+            this.roomname = name;
+        } else {
+            console.log("Room set default name");
+            this.roomname = "New Room";
+            console.log("room name: ", this.roomname);
+        }
+    }
+    setWalls(walls){
+        if(walls){
+            walls.forEach((wall)=>{
+                console.log("Room setWalls wall.direction", wall.getDirection());
+                let newWall = this.getDefaultWall(wall.getDirection());
+                newWall.setColor(wall.color);
+                newWall.setTextur(wall.textur);
+                newWall.setVisible(wall.visible);
+            });
 
-    setDefaults(){
-        this._id=null;
-        this.user_id=null;
-        this.roomname="New Room";
-        this.walls=[
-            new Wall('right'), new Wall('left'), new Wall('back'), new Wall('front'), new Wall('up'), new Wall('down')
-        ];
-        this.sky=null;
-        this.light=null
-        this.mediaobjects=[];
+        } else{
+           this.walls = [
+               this.getDefaultWall("right"),
+               this.getDefaultWall("left"),
+               this.getDefaultWall("back"),
+               this.getDefaultWall("front"),
+               this.getDefaultWall("up"),
+               this.getDefaultWall("down")
+           ];
+        }
+    }
+    setSky(sky){
+        if(sky){
+            this.sky = sky;
+        } else {
+            this.sky = null;
+        }
+    }
+    setLight(light){
+        if(light){
+            this.light = light;
+        } else {
+            this.light = null;
+        }
+    }
+    setMediaobjects(mediaobjects){
+        if(mediaobjects){
+            this.mediaobjects = mediaobjects;
+        } else {
+            this.mediaobjectsm = null;
+        }
+    }
+
+    setDefaults(user_id){
+        console.log("Room set Defaults");
+        this.setId(user_id);
+        this.setUserId(user_id);
+        this.setRoomName();
+        this.setWalls();
+        this.setSky();
+        this.setLight();
+        this.setMediaobjects();
+    }
+    getDefaultWall(direction){
+        return new Wall(null,direction);
+    }
+    loadFromDB(){
+
     }
 
 }
